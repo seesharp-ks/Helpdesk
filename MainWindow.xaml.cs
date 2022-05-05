3fd67788470e12b477ea801faa03802017f2cd9b
Nero <90371154+seesharp-ks@users.noreply.github.com>
2022-05-05 15:54:55 +0300

    050522 прототип + запрос
    up

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using helpdesk.Pages;
+
+namespace helpdesk
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); //https://metanit.com/sharp/wpf/5.10.php TabControl; https://www.wpf-tutorial.com/rich-text-controls/richtextbox-control/ RichTextBox; https://www.wpf-tutorial.com/rich-text-controls/how-to-creating-a-rich-text-editor/ RichTextEditor;
+            MainFrame.Navigate(new DividedPage());
+            //https://www.codeproject.com/Articles/30134/LINQ-group-by-and-WPF-Data-Binding -- Список
+        }
+    }
+}
---
+++ Pages/DividedPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace helpdesk.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для DividedPage.xaml
+    /// </summary>
+    public partial class DividedPage : Page
+    {
+        public DividedPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Open_Executed(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(rtbAnswer.Document.ContentStart, rtbAnswer.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        } //использовать заранее созданные текстовые документы, но нужно подумать над выгрузкой из памяти уже существующие
+
+        private void Save_Executed(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(rtbAnswer.Document.ContentStart, rtbAnswer.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void lviWelcome_Selected(object sender, RoutedEventArgs e)
+        {
+            FileStream fileStream = new FileStream("../../BasedText/Welcome.rtf", FileMode.Open);
+            TextRange range = new TextRange(rtbAnswer.Document.ContentStart, rtbAnswer.Document.ContentEnd);
+            range.Load(fileStream, DataFormats.Rtf);
+            fileStream.Close();
+        }
+
+        private void lviAddEditDelete_Selected(object sender, RoutedEventArgs e)
+        {
+            FileStream fileStream = new FileStream("../../BasedText/AddEditDelete.rtf", FileMode.Open);//@"BasedText\\AddEditDelete.rtf
+            TextRange range = new TextRange(rtbAnswer.Document.ContentStart, rtbAnswer.Document.ContentEnd);
+            range.Load(fileStream, DataFormats.Rtf);
+            fileStream.Close();
+        }
+    }
+}
