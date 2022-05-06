284c9e151501fbc383ddf495e9b2cc341cdcbee7
Nero <90371154+seesharp-ks@users.noreply.github.com>
2022-05-06 16:51:58 +0300

    UPD, but hash comparison doesn't work
    tilt

--- Pages/DividedPage.xaml.cs
+++ Pages/DividedPage.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace helpdesk.Pages
 {
@@ -70,6 +59,11 @@ namespace helpdesk.Pages
             TextRange range = new TextRange(rtbAnswer.Document.ContentStart, rtbAnswer.Document.ContentEnd);
             range.Load(fileStream, DataFormats.Rtf);
             fileStream.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Windows/AuthWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using helpdesk.Baza;
+using System.Security.Cryptography;
+
+namespace helpdesk.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window {
+        Entities Entities = new Entities();
+        public static Account acc;
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuthorize_Click(object sender, RoutedEventArgs e)
+        {
+            acc = Entities.Account.FirstOrDefault(i => i.Login == tbLogin.Text && i.Password == pbPass.Password);
+            if (acc != null)
+            {
+                MainWindow mw = new MainWindow();
+                mw.Show();
+                Hide();
+            }
+            else
+            {
+                if (tbLogin.Text.Length == 0 && pbPass.Password.Length != 0)
+                    MessageBox.Show("Вы не ввели пароль.", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+                else if (tbLogin.Text.Length == 0 && pbPass.Password.Length == 0)
+                    MessageBox.Show("Вы не заполнили данные для авторизации.", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+                else if (tbLogin.Text.Length != 0 && pbPass.Password.Length != 0)
+                    MessageBox.Show("В доступе отказано. Проверьте правильность введенных данных.", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+                else if (tbLogin.Text.Length == 0 && pbPass.Password.Length != 0)
+                    MessageBox.Show("Вы не ввели логин.", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private string enhash(string text)
+        {
+            byte[] data = Encoding.Default.GetBytes(text);
+            var result = new SHA256Managed().ComputeHash(data);
+            return BitConverter.ToString(result).Replace("-", "").ToLower(); //сравнить хэши со значением в БД и готово
+        }
+    }
+}
